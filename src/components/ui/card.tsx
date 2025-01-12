c7b02e285401d75a12784f6166395f5323a3306b
Praveen <111781869+pscode22@users.noreply.github.com>
2025-01-13 01:25:30 +0530

    Homepage

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,82 +1,78 @@
-import { rentalData } from '@/lib/data';
+import { Property } from '@/types';
 import millify from 'millify';
 import { BsGridFill } from 'react-icons/bs';
 import { GoUnverified, GoVerified } from 'react-icons/go';
 import { IoBedOutline } from 'react-icons/io5';
 import { TbBath } from 'react-icons/tb';
 
-export default function Card() {
+export default function Card({ item }: { item: Property }) {
   return (
-    <section className="grid grid-cols-3 lg:grid-cols-4 gap-5">
-      {rentalData.hits.map((item) => (
-        <div className="card" key={item.id}>
-          <div
-            className="img bg-no-repeat bg-cover"
-            style={{ backgroundImage: `url(${item.coverPhoto.url})` }}
-          />
+    <div className="card" key={item.id}>
+      <div
+        className="img bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${item.coverPhoto.url})` }}
+      />
 
-          <div className="badge">
-            {item.isVerified ? (
-              <GoVerified size={11} />
-            ) : (
-              <GoUnverified size={11} />
-            )}
-            {item.isVerified ? 'Verified' : 'Not Verified'}
-          </div>
+      <div className="badge">
+        {item.isVerified ? (
+          <GoVerified size={11} />
+        ) : (
+          <GoUnverified size={11} />
+        )}
+        {item.isVerified ? 'Verified' : 'Not Verified'}
+      </div>
 
-          <div className="px-5 mt-5 text-black relative block">
-            <div>
-              <div className="propertyInfo">
-                <p className="font-semibold text-[1.375rem] text-[#5a4dea]">
-                  AED {millify(item.price)}
-                  <span className="text-xs ml-px text-secondary">/month</span>
-                </p>
+      <div className="px-5 mt-5 text-black relative block">
+        <div>
+          <div className="propertyInfo">
+            <p className="font-semibold text-[1.375rem] text-[#5a4dea]">
+              AED {millify(item.price)}
+              <span className="text-xs ml-px text-secondary">/month</span>
+            </p>
 
-                <p className="my-3 font-bold text-lg min-h-3 flex align-middle">
-                  {item.title.length <= 40
-                    ? item.title
-                    : item.title.slice(0, 41) + '...'}
-                </p>
+            <p className="my-3 font-bold text-lg min-h-3 flex align-middle">
+              {item.title.length <= 40
+                ? item.title
+                : item.title.slice(0, 41) + '...'}
+            </p>
 
-                {/* <FavBtn
+            {/* <FavBtn
                   id={item.id}
                   favCards={favCards}
                   handleClick={handleClick}
                 /> */}
-              </div>
+          </div>
 
-              <div className="flex flex-wrap text-xs mb-2.5 mt-1.5 text-secondary h-8 align-middle">
-                <p>
-                  <span style={{ fontWeight: 'bold' }}>Agency :-</span>{' '}
-                  {item.agency.name}
-                </p>
-              </div>
-            </div>
-
-            <hr />
-
-            <footer
-              className="text-xs font-medium text-secondary  grid place-items-center gap-[0.3125rem] mt-3"
-              style={{ gridTemplateColumns: 'auto auto auto' }}
-            >
-              <p className="flex gap-[0.1875rem] align-middle ">
-                <IoBedOutline className="text-[0.9375rem] text-primary" />
-                {item.rooms} Bed{item.rooms > 1 ? 's' : null}
-              </p>
-
-              <p className="flex gap-[0.1875rem] align-middle ">
-                <TbBath className="text-[0.9375rem] text-primary" />
-                {item.baths} Bathroom{item.baths > 1 ? 's' : null}
-              </p>
-
-              <p className="flex gap-[0.1875rem] align-middle ">
-                <BsGridFill className="text-[0.9375rem] text-primary" />
-                {item.area.toFixed(2)} sqft
-              </p>
-            </footer>
+          <div className="flex flex-wrap text-xs mb-2.5 mt-1.5 text-secondary h-8 align-middle">
+            <p>
+              <span style={{ fontWeight: 'bold' }}>Agency :-</span>{' '}
+              {item.agency.name}
+            </p>
           </div>
         </div>
-      ))}
-    </section>
+
+        <hr />
+
+        <footer
+          className="text-xs font-medium text-secondary  grid place-items-center gap-[0.3125rem] mt-3"
+          style={{ gridTemplateColumns: 'auto auto auto' }}
+        >
+          <p className="flex gap-[0.1875rem] align-middle ">
+            <IoBedOutline className="text-[0.9375rem] text-primary" />
+            {item.rooms} Bed{item.rooms > 1 ? 's' : null}
+          </p>
+
+          <p className="flex gap-[0.1875rem] align-middle ">
+            <TbBath className="text-[0.9375rem] text-primary" />
+            {item.baths} Bathroom{item.baths > 1 ? 's' : null}
+          </p>
+
+          <p className="flex gap-[0.1875rem] align-middle ">
+            <BsGridFill className="text-[0.9375rem] text-primary" />
+            {item.area.toFixed(2)} sqft
+          </p>
+        </footer>
+      </div>
+    </div>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,17 @@
-import Card from "@/components/ui/card";
+import Card from '@/components/ui/card';
+import { rentalData } from '@/lib/data';
 
 export default function Home() {
   return (
-    <main className="flex align-middle justify-center">
-      <Card />
+    <main>
+      <section className="mt-10 mb-5 w-100 bg-white h-[5.125rem]">Location</section>
+
+      {/* Property Cards */}
+      <section className="cardContainer">
+        {rentalData.hits.map((item) => (
+          <Card item={item} />
+        ))}
+      </section>
     </main>
-  )
+  );
 }
